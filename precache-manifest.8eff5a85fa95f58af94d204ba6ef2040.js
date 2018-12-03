3da572460f6de1cc87a2556d3404e3818d504705
Sambath Prum <psb.sambath@gmail.com>
2018-12-02 22:43:28 -0600

    Updates

---
+++ precache-manifest.8eff5a85fa95f58af94d204ba6ef2040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "dba951b28d06e2bb9b38",
+    "url": "/robofriends/static/js/main.dba951b2.chunk.js"
+  },
+  {
+    "revision": "fb2f3d78a3d476a10f86",
+    "url": "/robofriends/static/js/1.fb2f3d78.chunk.js"
+  },
+  {
+    "revision": "dba951b28d06e2bb9b38",
+    "url": "/robofriends/static/css/main.cd6a4291.chunk.css"
+  },
+  {
+    "revision": "fb2f3d78a3d476a10f86",
+    "url": "/robofriends/static/css/1.d9ab1075.chunk.css"
+  },
+  {
+    "revision": "4cf585ced360bb2d7ac0fd714424aef9",
+    "url": "/robofriends/index.html"
+  }
+];
